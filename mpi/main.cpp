fa2aa1039f07c3b7084751b4780e8923402fdfbf
gintaras <studtscm05@calc.cod.phystech.edu>
2023-10-25 14:36:47 +0300

    main file

--- mpi/main.cpp
+++ mpi/main.cpp
@@ -10,23 +10,9 @@ int main(int argc, char** argv) {
     ParseCommandLineArguments(argc, argv);
     init_cart_comm(argc, argv);
 
-    // Create particles
     GeneratePositions_MPI();
-    std::mt19937 rng;
-    GenerateVelocities(rng);
-
-    auto v = __get_neighbor_particles_MPI();
-
-    cout << MPI_OPTIONS.rank << "\n";
-    for (int i = 0; i < v.size(); ++i) {
-        for (const auto& p : v[i])
-            cout << p.x << ' ' << p.y << ' ' << p.z << "\n";
-        cout << "\n";
-    }
-
-    // // Write particles
-    // ofstream out;
-    // WriteParticlesXYZ_MPI(out);
+    
+    Simulate_MPI();
 
     // finilize mpi
     finilize_cart_comm();
